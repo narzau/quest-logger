fd8b5a2d5d5febcf68d50101eea90d6d85c3114d
Ignacio Arzaut <arzautignacio@gmail.com>
2025-07-22 11:39:27 -0300

    asd

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
-
+    
     # Server settings
     SERVER_NAME: str = "localhost"
     SERVER_HOST: str = "http://localhost:8000"
